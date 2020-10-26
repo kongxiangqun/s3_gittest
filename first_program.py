fb8c0da76f3b765c40dc9d144d30011014c88079
kxq <17833333641@163.com>
2020-10-26 17:45:21 +0800

    change on master

--- first_program.py
+++ first_program.py
@@ -2,6 +2,7 @@ def sayhi(name):
     print('Hello world',name)
     CHANGE AGAIN
     changed on linux
+    change on master
     print('Hecond change')
     change after rollback
     la la la la la
